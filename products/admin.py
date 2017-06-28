76f8272b3196ed02d5c142884a16877cdd47f569
Challenge Happy <happy@Challengeui-MBP.lan>
2017-06-28 18:16:38 +0800

    초기 모델 완성'

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Provider, Tag, Contact, Category, Product, CompanyProduct
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Provider
+    list_display = ['cn_name', 'en_name']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Tag
+    list_display = ['tag', ]
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Contact
+    list_display = ['cn_name', 'role', 'email', 'mobile', 'wechat', 'qq']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Category
+    list_display = ['cn_name', 'en_name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+    list_display = ['cn_name', 'en_name', 'category']
+
+@admin.register(CompanyProduct)
+class CompanyProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = CompanyProduct
+    list_display = ['provider', 'product', 'created_at']
+
+
+
--- products/models.py
+++ products/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Provider(models.Model):
     """docstring for Provider"""
     """ 설명 """
-    cn_name = models.CharField(max_length=50)
-    en_name = models.CharField(max_length=50)
-    cn_address = models.CharField(max_length=100, blank=True, null=True)
-    en_address = models.CharField(max_length=100, blank=True, null=True)
+    cn_name = models.CharField(max_length=50, verbose_name='CHINESE NAME')
+    en_name = models.CharField(max_length=50, verbose_name='ENGLISH NAME')
+    cn_address = models.CharField(max_length=100, blank=True, null=True, verbose_name='CHINESE ADDRESS')
+    en_address = models.CharField(max_length=100, blank=True, null=True, verbose_name='ENGLISH ADDRESS')
     homepage = models.URLField(blank=True, null=True)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -40,14 +40,15 @@ class Contact(models.Model):
         (SALESMAN, 'SalesMan'),
     )
 
-    cn_name = models.CharField(max_length=30)
-    en_name = models.CharField(max_length=30, blank=True, null=True)
+    cn_name = models.CharField(max_length=30, verbose_name='CHINESE NAME')
+    en_name = models.CharField(max_length=30, blank=True, null=True, verbose_name='ENGLISH NAME')
     role = models.CharField(max_length=2, choices=ROLE, default=SALESMAN)
 
     email = models.EmailField(blank=True, null=True)
     mobile = models.CharField(max_length=30, blank=True, null=True)
     fixed = models.CharField(max_length=30, blank=True, null=True)
     wechat = models.CharField(max_length=30, blank=True, null=True)
+    qq = models.CharField(max_length=30, blank=True, null=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -55,7 +56,7 @@ class Contact(models.Model):
         return self.cn_name
 
 
-class Category(object):
+class Category(models.Model):
     """docstring for Category"""
     cn_name = models.CharField(max_length=30)
     en_name = models.CharField(max_length=30)
@@ -67,32 +68,33 @@ class Category(object):
 class Product(models.Model):
     """docstring for Product"""
     """ 설명 """
-    cn_name = models.CharField(max_length=100)
-    en_name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category)     # Category
+    cn_name = models.CharField(max_length=100, verbose_name='CHINESE NAME')
+    en_name = models.CharField(max_length=100, verbose_name='ENGLISH NAME')
+    cas_no = models.CharField(max_length=20, verbose_name='CAS NO', blank=True, null=True)
+    category = models.ForeignKey(Category)
 
     cn_hscode = models.CharField(max_length=15, verbose_name='중국 HS CODE')
     ko_hscode = models.CharField(max_length=15, verbose_name='한국 HS CODE')
-    etc_hscode = models.CharField(max_length=15, verbose_name='ETC HS CODE')
+    etc_hscode = models.CharField(max_length=15, verbose_name='ETC HS CODE', blank=True, null=True)
 
-    molnumber = models.CharField(max_length=100, verbose_name='분자량')
-    chemequal = models.CharField(max_length=100, verbose_name='화학식')
-    chemstructure = models.ImageField(verbose_name='구조식', blank=True, null=True)
+    molnumber = models.FloatField(max_length=100, verbose_name='분자량', blank=True, null=True)
+    chemequal = models.CharField(max_length=100, verbose_name='화학식', blank=True, null=True)
+    chemstructure = models.ImageField(verbose_name='구조식', blank=True, null=True, upload_to='chemicalstructure/')
     
-    usage = models.CharField(max_length=500, verbose_name='용도')
+    usage = models.TextField(max_length=500, verbose_name='용도', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.cn_name
 
     
-class CompanyProduct(object):
-    """docstring for CompanyProduct"""
-    company = models.ForeignKey(Company)
+class CompanyProduct(models.Model):
+    """docstring for CompcanyProduct"""
+    provider = models.ForeignKey(Provider)
     product = models.ForeignKey(Product)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} {}".format(self.company, self.product)
+        return "{} {}".format(self.provider, self.product)
         
